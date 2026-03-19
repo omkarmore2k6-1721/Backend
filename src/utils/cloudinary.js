3d0db92e605efaaa86251b92466ad91baf73a0bc
Omkar More <omkarmore2k6@gmail.com>
2026-03-19 19:26:28 +0530

    multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+const uploadToCloudinary = async (localFilePath) => {
+    try{
+        const result = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+        
+        console.log("Cloudinary upload result: ", result.url);
+        fs.unlinkSync(localFilePath);
+        return result;
+    }catch(error){
+        fs.unlinkSync(localFilePath);
+        console.error("Error uploading to Cloudinary: ", error);
+        throw error;
+    }
+
+}
+
+export default uploadToCloudinary;
